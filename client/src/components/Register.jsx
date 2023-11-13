9db4e1cb477d3d55ab2f5385d12348ae9340046b
Khouloudlaarous <khouloudaissa@gmail.com>
2023-11-14 00:09:35 +0100

    add forms to signin and register pages

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
+    <div className="register-container">
+      <img src="../img/icons/PhotoBomb.jpg" alt="PhotoBomb Logo" className="logo" />
+      <p className="logo-text">PhotoBomb!</p>
+      <form>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name:</label>
+          <input type="text" id="firstName" name="firstName" placeholder="Enter your first name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name:</label>
+          <input type="text" id="lastName" name="lastName" placeholder="Enter your last name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" />
+        </div>
+        <Link to="/signin">Sign in</Link>
+        <button type="submit" className="register-button">Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-return (
-    <h1>SIgnIn</h1>
-)
+    return (
+    <div className="sign-in-container">
+    <img src="../img/icons/PhotoBomb.jpg" alt="PhotoBomb Logo" className="logo" />
+    <p className="logo-text">PhotoBomb!</p>
+        <form>
+        <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" />
+        </div>
+        <Link to="/register">Create Account</Link>
+        <button type="submit" className="sign-in-button">Sign In</button>
+        </form>
+    </div>
+);
 }
 
-export default SignIn
+export default SignIn;
